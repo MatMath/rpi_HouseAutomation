b57042063638bec52d2f8c0e2210771c469cdf1f
MatMath <mathieu.k.legault@gmail.com>
2017-04-30 13:10:43 -0400

    Adding back the manual testing

---
+++ manualTestGpioFlow.js
@@ -0,0 +1,24 @@
+const { openBlindSequence, closeBlindSequence } = require('./src/blindActions');
+const { listenOnPinChange } = require('./src/gpioActions');
+const { openLight } = require('./src/lightAction');
+const config = require('./config.json');
+
+const openBlindX = (motorIndex) => {
+  openBlindSequence(config.blindMotorControl[motorIndex]);
+};
+
+const closeBlindX = (motorIndex) => {
+  closeBlindSequence(config.blindMotorControl[motorIndex]);
+};
+
+const pinEventEmitter = () => {
+  listenOnPinChange();
+};
+
+pinEventEmitter();
+// openBlindX(1);
+openLight();
+
+module.exports.openBlindX = openBlindX;
+module.exports.closeBlindX = closeBlindX;
+module.exports.openLight = openLight;
