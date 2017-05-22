61af67dadea8587346bd23890cb5d5caf56ae617
MatMath <mathieu.k.legault@gmail.com>
2017-05-22 17:45:30 -0400

    More test with fswebcam

--- test/flowTesting.js
+++ test/flowTesting.js
@@ -5,6 +5,8 @@ const { diskUtility } = require('../config.json');
 const fs = require('fs');
 
 const now = Date.now();
-fs.mkdirSync(`${diskUtility.saveImgPath}/testing`);
-execSync(`sudo fswebcam -v -r 1280x720 --no-banner video/testing/${now}.jpg`); // this will return even if the img is not saved.
+const testDir = `${diskUtility.saveImgPath}/testing`;
+if (!fs.existsSync(testDir)) { fs.mkdirSync(testDir); }
+execSync(`sudo fswebcam -v -r 1280x720 -S 20 --no-banner video/testing/${now}.jpg`); // This skip 20 img (stability) then save 1 so lose 1.5 sec :/
+
 execSync('aws s3  sync  ./video/  s3://backupforpi/video/');
