074a30ea7a2a7436683710dde9477ba0ce41a5c2
cesine <cesine@yahoo.com>
2018-08-04 12:16:15 -0400

    add stack trace to logs
    and log uncaught exceptions

--- src/middleware/error.js
+++ src/middleware/error.js
@@ -1,10 +1,30 @@
+process
+  .on('unhandledRejection', (reason, p) => {
+    console.error(reason, 'Unhandled Rejection at Promise', p);
+  })
+  .on('uncaughtException', (err) => {
+    console.error(err, 'Uncaught Exception thrown');
+    process.exit(1);
+  });
+
+
 // Express error handler must have 4 args
 // eslint-disable-next-line no-unused-vars
 module.exports = (err, req, res, next) => {
   res.status(err.status || 500);
+
+  req.log.fields.stack = err.stack;
+  if (process.env.NODE_ENV === 'production' && err.status >= 500) {
+    res.json({
+      message: 'Internal Server Error',
+      status: 500,
+    });
+    return;
+  }
+
   res.json({
     message: err.message,
     error: {},
-    title: 'error',
+    title: 'Error',
   });
 };
