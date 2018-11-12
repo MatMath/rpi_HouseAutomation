be771b7227c7245873c7b37a35298582ae59c744
Mathieu <mathieu.k.legault@gmail.com>
2018-11-12 20:49:53 +0000

    style(ESLint): ESLint fix

--- src/mongodbHandler.js
+++ src/mongodbHandler.js
@@ -1,6 +1,6 @@
 // Need 2 DB, Door movement & Front movement.
 // Generic libs
-const { MongoClient, MongoError } = require('mongodb');
+const { MongoClient } = require('mongodb');
 
 // Custom fnct
 const { mongourl, mongoDBName } = require('config');
@@ -23,46 +23,50 @@ const dBconnect = async () => {
 const addFrontMovementLog = () => {
   const collection = dbName.collection('frontmovement');
   // Insert some documents
-  return collection.insertOne({timestamp: Date.now()})
-  .catch(error => {
-    log.error({ fnct: 'Mongo Front movement', error }, 'Err pushing to Mongo')
+  return collection.insertOne({ timestamp: Date.now() })
+  .catch((error) => {
+    log.error({ fnct: 'Mongo Front movement', error }, 'Err pushing to Mongo');
   });
-}
+};
 
 const getFrontMovement = (date) => {
   const latest = date || Date.now() - 604800000; // 1 week
   // Date need to be in JS numeric format only.
-  return new Promise(function(resolve, reject) {
+  return new Promise((resolve, reject) => {
     // for some reason Mongo send a first "resolve" before it is time.
     dbName.collection('frontmovement')
-    .find({"timestamp": { $gte : latest }})
-    .toArray((err, results) => {
-      if (err) { return log.error({ fnct: 'Mongo GET Front movement', error }, 'Err Get of Mongo'); }
-      resolve(results);
+    .find({ timestamp: { $gte: latest } })
+    .toArray((error, results) => {
+      if (error) {
+        log.error({ fnct: 'Mongo GET Front movement', error }, 'Err Get of Mongo');
+        return reject(error);
+      }
+      return resolve(results);
     });
   });
-
 };
 
-const addDoorMovementLog = () => {
+const addDoorMovementLog = () =>
   // Insert some documents
-  return dbName.collection('doormovement')
-  .insertOne({timestamp: Date.now()})
+   dbName.collection('doormovement')
+  .insertOne({ timestamp: Date.now() })
   .catch(error => log.error({ fnct: 'Mongo Door movement', error }, 'Err pushing to Mongo'));
-}
 
 const getDoorMovement = (date) => {
   const latest = date || Date.now() - 604800000; // 1 week
-  return new Promise(function(resolve, reject) {
+  return new Promise((resolve, reject) => {
     // for some reason Mongo send a first "resolve" before it is time.
     dbName.collection('doormovement')
-    .find({"timestamp": { $gte : latest }})
-    .toArray((err, results) => {
-      if (err) { return log.error({ fnct: 'Mongo GET Door movement', error }, 'Err Get of Mongo'); }
-      resolve(results);
+    .find({ timestamp: { $gte: latest } })
+    .toArray((error, results) => {
+      if (error) {
+        log.error({ fnct: 'Mongo GET Door movement', error }, 'Err Get of Mongo');
+        return reject(error);
+      }
+      return resolve(results);
     });
   });
-}
+};
 
 module.exports = {
   dBconnect,
